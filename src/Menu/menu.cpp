bc09015e4a80e6369f74b0daac85b551b7341232
Franz Alarcon <franzalarconv@gmail.com>
2022-07-21 21:03:29 -0700

    Add newline at end of files

--- src/Menu/menu.cpp
+++ src/Menu/menu.cpp
@@ -11,3 +11,88 @@
 // Dependencies: Box class and ConsoleEditor class.
 //------------------------------------------------------------------------------
 
+#include "Menu/menu.h"
+
+
+namespace conu {
+
+//------------------------------------------------------------------------------
+// Static member initialization
+ConsoleEditor& Menu::console = ConsoleEditor::getInstance();
+
+//------------------------------------------------------------------------------
+Menu::Menu() :
+    container{ VertContainer() },
+    exitMenu{ false },
+    exitReply{ Reply::CONTINUE } {
+
+}
+
+//------------------------------------------------------------------------------
+Menu::~Menu() {
+
+}
+
+//------------------------------------------------------------------------------
+ItemAccessor Menu::operator [] (int layer) {
+    return container[layer];
+}
+
+//------------------------------------------------------------------------------
+Reply Menu::enter() {
+    // TODO: Add push_back to future renderer
+    entryLoop();
+    // TODO: Add pop to future renderer
+
+    return exitReply;
+}
+
+//------------------------------------------------------------------------------
+void Menu::print() {
+    if (options.useBuffering) {
+        container.buffer(Position{ 0, 0 }, console.getWindowBoundary());
+        console.printWriteBuffer();
+        return;
+    }
+
+    container.draw(Position{ 0, 0 }, console.getWindowBoundary());
+}
+
+//------------------------------------------------------------------------------
+void Menu::exit() {
+    exitMenu = true;
+}
+
+//------------------------------------------------------------------------------
+void Menu::setOptions(const MenuOptions& options) {
+    this->options = options;
+}
+
+//------------------------------------------------------------------------------
+const MenuOptions& Menu::getOptions() const {
+    return options;
+}
+
+//------------------------------------------------------------------------------
+VertContainer& Menu::getContainer() {
+    return container;
+}
+
+//------------------------------------------------------------------------------
+void Menu::setExitReply(Reply exitReply) {
+    this->exitReply = exitReply;
+}
+
+//------------------------------------------------------------------------------
+void Menu::entryLoop() {
+    exitMenu = false;
+
+    while (!exitMenu) {
+        conu::InputEvent input = console.getButtonInput();
+
+
+
+    }
+}
+
+}
--- src/Menu/menu.h
+++ src/Menu/menu.h
@@ -8,6 +8,77 @@
 // Class Functionality:
 //     - 
 // 
-// Dependencies: Box class and ConsoleEditor class.
+// Dependencies: Box, ConsoleEditor, and MenuReplyActionFactory class.
 //------------------------------------------------------------------------------
 
+#pragma once
+
+#include "ConsoleEditor/consoleeditor.h"
+#include "Box/box.h"
+#include "Box/BoxContainer/vertcontainer.h"
+#include "menureplyaction/menureplyactionfactory.h"
+
+namespace conu {
+
+//------------------------------------------------------------------------------
+// 
+struct MenuOptions {
+    bool useBuffering;
+    bool useAutoPrint;
+    int frameRate;
+
+    MenuOptions() :
+        useBuffering{ true },
+        useAutoPrint{ true },
+        frameRate{ 30 } {
+    
+    }
+};
+
+//------------------------------------------------------------------------------
+// 
+class Menu {
+public:
+    Menu();
+    virtual ~Menu();
+
+    ItemAccessor operator [] (int layer);
+
+    // Enter the Menu operation
+    // Blocks until the Menu exits
+    Reply enter();
+
+    // Mark for the Menu to exit operation
+    void exit();
+
+    // Print the Menu contents to the console screen
+    // Uses draw or buffer depending on menu options
+    void print();
+
+    // Set the current options of the menu
+    void setOptions(const MenuOptions& options);
+
+    // Get the current options of the menu
+    const MenuOptions& getOptions() const;
+
+    // Get a reference to the main vertical box container of the menu
+    VertContainer& getContainer();
+
+    // Set the Reply that is returned by the menu upon exit from enter()
+    void setExitReply(Reply exitReply);
+
+private:
+    static ConsoleEditor& console;
+    static MenuReplyActionFactory actionFactory;
+
+    VertContainer container;
+    bool exitMenu;
+    Reply exitReply;
+
+    MenuOptions options;
+
+    void entryLoop();
+
+};
+
+}
