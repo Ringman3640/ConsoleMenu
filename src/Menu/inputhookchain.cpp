503bb9dd8c07064bcdaea35842bc656e0e240a19
Franz Alarcon <franzalarconv@gmail.com>
2022-07-31 00:58:14 -0700

    Add InputHookChain class

---
+++ src/Menu/inputhookchain.cpp
@@ -0,0 +1,132 @@
+//------------------------------------------------------------------------------
+// inputhookchain.cpp
+// Implementation for the InputHookChain class
+// Author: Franz Alarcon
+//------------------------------------------------------------------------------
+// Description: 
+//
+// Class Functionality:
+//     - 
+// 
+// Dependencies: InputEvent struct.
+//------------------------------------------------------------------------------
+
+#include "Menu/inputhookchain.h"
+
+namespace conu {
+
+//------------------------------------------------------------------------------
+HookHandle::HookHandle() :
+    hookPtr{ nullptr } {
+
+}
+
+//------------------------------------------------------------------------------
+HookHandle::HookHandle(std::function<void(InputEvent&)>* inHandle) :
+    hookPtr{ inHandle } {
+    
+}
+
+//------------------------------------------------------------------------------
+InputHookChain::Node::Node() :
+    prev{ nullptr },
+    next{ nullptr },
+    hook{ } {
+
+}
+
+//------------------------------------------------------------------------------
+InputHookChain::InputHookChain() :
+    head{ nullptr },
+    tail{ nullptr },
+    count{ 0 } {
+
+}
+
+//------------------------------------------------------------------------------
+InputHookChain::~InputHookChain() {
+    clear();
+}
+
+//------------------------------------------------------------------------------
+HookHandle InputHookChain::addInputHook(std::function<void(InputEvent&)> hook) {
+    Node* oldHead = head;
+    head = new Node();
+    head->hook = hook;
+    head->next = oldHead;
+
+    if (oldHead != nullptr) {
+        oldHead->prev = head;
+    }
+
+    ++count;
+    return HookHandle(&head->hook);
+}
+
+//------------------------------------------------------------------------------
+bool InputHookChain::removeInputHook(HookHandle& handle) {
+    // Check handle or empty chain
+    if (handle.hookPtr == nullptr || count == 0) {
+        return false;
+    }
+
+    // Search for hook in chain
+    Node* traversal = head;
+    while (true) {
+        if (traversal == nullptr) {
+            return false;
+        }
+
+        if (&traversal->hook == handle.hookPtr) {
+            break;
+        }
+
+        traversal = traversal->next;
+    }
+
+    // Remove hook
+    if (count == 1) {               // Removal node is head and tail
+        head = nullptr;
+        tail = nullptr;
+    }
+    else if (traversal == head) {   // Removal node is only head
+        head = traversal->next;
+        head->prev = nullptr;
+    }
+    else if (traversal == tail) {   // Removal node is only tail
+        tail = traversal->prev;
+        tail->next = nullptr;
+    }
+    else {                          // Removal node is an internal
+        traversal->next->prev = traversal->prev;
+        traversal->prev->next = traversal->next;
+    }
+    delete traversal;
+    handle.hookPtr = nullptr;
+    --count;
+    return true;
+}
+
+//------------------------------------------------------------------------------
+void InputHookChain::startHookChain(InputEvent& input) const {
+    Node* traversal = head;
+    while (traversal != nullptr && input.type != inputEvent::Type::INVALID) {
+        traversal->hook(input);
+        traversal = traversal->next;
+    }
+}
+
+//------------------------------------------------------------------------------
+void InputHookChain::clear() {
+    Node* traversal = head;
+
+    while (traversal != nullptr) {
+        Node* nextNode = traversal->next;
+        delete traversal;
+        traversal = nextNode;
+    }
+
+    count = 0;
+}
+
+}
---
+++ src/Menu/inputhookchain.h
@@ -0,0 +1,93 @@
+//------------------------------------------------------------------------------
+// inputhookchain.h
+// Interface for the InputHookChain class
+// Author: Franz Alarcon
+//------------------------------------------------------------------------------
+// Description: 
+//
+// Class Functionality:
+//     - 
+// 
+// Dependencies: InputEvent struct.
+//------------------------------------------------------------------------------
+
+#pragma once
+
+#include <functional>
+#include "ConsoleEditor/inputevent.h"
+
+namespace conu {
+
+//------------------------------------------------------------------------------
+// HookHandle class
+// Defines a handle to a specific input hook handle within an InputHookChain
+//     object.
+// Used to remove an inserted hook from an InputHookChain.
+class HookHandle {
+    friend class InputHookChain;
+
+public:
+    //--------------------------------------------------------------------------
+    // Default constructor
+    HookHandle();
+
+    //--------------------------------------------------------------------------
+    // Parameter constructor
+    HookHandle(std::function<void(InputEvent&)>* inHandle);
+
+private:
+    // Pointer to handle function
+    std::function<void(InputEvent&)>* hookPtr;
+
+};
+
+//------------------------------------------------------------------------------
+class InputHookChain {
+public:
+
+    //--------------------------------------------------------------------------
+    // Default constructor
+    InputHookChain();
+
+    //--------------------------------------------------------------------------
+    // Destructor
+    ~InputHookChain();
+
+    //--------------------------------------------------------------------------
+    // Add an input hook to the start of the hook chain.
+    // Returns a handle to the inserted hook to use for removal.
+    HookHandle addInputHook(std::function<void(InputEvent&)> hook);
+
+    //--------------------------------------------------------------------------
+    // Remove an inserted hook from the hook chain given the hook's handle.
+    // Returns false if the HookHandle was not found. Otherwise returns true.
+    bool removeInputHook(HookHandle& handle);
+
+    //--------------------------------------------------------------------------
+    // Execute the hook chain given an InputEvent struct.
+    void startHookChain(InputEvent& input) const;
+
+    //--------------------------------------------------------------------------
+    // Clear all hooks from the hook chain.
+    void clear();
+
+private:
+    // Node struct
+    // Helper structure for the hook chain linked list
+    struct Node {
+        Node* prev;
+        Node* next;
+        std::function<void(InputEvent&)> hook;
+
+        Node();
+
+    };
+
+    // Linked list implementation for the node chain
+    Node* head;
+    Node* tail;
+    int count;
+
+};
+
+}
